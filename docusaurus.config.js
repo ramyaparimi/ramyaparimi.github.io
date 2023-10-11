7edbee990f6f30cd21f3a9d67ea1045ea3182ab3
Ramya Parimi <33761166+ramyaparimi@users.noreply.github.com>
2023-10-11 06:54:26 -0500

    modified:   docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,6 +20,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'ramyaparimi', // Usually your GitHub org/user name.
+  deploymentBranch: 'gh-pages',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
