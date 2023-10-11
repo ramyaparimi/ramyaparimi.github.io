32a2ba3ab4d75a787ed4812c07020c30733602a3
Ramya Parimi <33761166+ramyaparimi@users.noreply.github.com>
2023-10-11 06:23:24 -0500

    fixed links and error for hg-deploy

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,15 +14,12 @@ const config = {
   url: 'https://ramyaparimi.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/portfolio-docusaurus',
-  projectName: 'https://ramyaparimi.github.io/portfolio-docusaurus/',
-  organizationName: 'ramyaparimi',
+  baseUrl: '/portfolio-docusaurus.io/',
+  projectName: 'portfolio-docusaurus',
   trailingSlash: false,
-
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'ramyaparimi', // Usually your GitHub org/user name.
-  projectName: 'https://github.com/ramyaparimi/portfolio-docusaurus', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
