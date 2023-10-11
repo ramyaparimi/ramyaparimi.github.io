b267b4567846ff876eb542994bdc99ba9d7ee098
Ramya Parimi <33761166+ramyaparimi@users.noreply.github.com>
2023-10-11 06:31:08 -0500

    changed congif.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
   url: 'https://ramyaparimi.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/portfolio-docusaurus.io/',
+  baseUrl: '/portfolio-docusaurus/',
   projectName: 'portfolio-docusaurus',
   trailingSlash: false,
   // GitHub pages deployment config.
