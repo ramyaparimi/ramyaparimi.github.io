dc03be4cac45e0b0f9bc57bf07ff87bb7adcf2fd
Ramya Parimi <33761166+ramyaparimi@users.noreply.github.com>
2023-10-11 07:40:58 -0500

    changes to enable deploy to gh pages

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,16 +11,16 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://ramyaparimi.github.io',
+  url: 'https://ramyaparimi.github.io', 
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
   projectName: 'ramyaparimi.github.io',
-  trailingSlash: false,
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'ramyaparimi', // Usually your GitHub org/user name.
-
+  trailingSlash: false,
+  deploymentBranch: 'gh-pages',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
